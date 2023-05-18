199081311be28b4def4b851367e7dc0568d2ff56
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-17 21:17:38 -0300

    Cors config

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,13 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 app.register(memoriesRoutes)
+
+app.register(cors, {
+    origin: true,
+})
 
 app.listen({
     port: 3333,
