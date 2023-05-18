92fe32349bf01080116064a2db582f1c14226974
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-17 21:03:25 -0300

    CRUD memories

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,8 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
-
-app.get('/users', async (response) => {
-    const users = await prisma.user.findMany()
-    return users
-})
+app.register(memoriesRoutes)
 
 app.listen({
     port: 3333,
