307cdb29908a8741cccd1f05e8a8d7942ccd7fb6
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-17 22:49:00 -0300

    Create separated components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import logo from '../assets/logo.png'
+import { EmptyMemories } from '../components/EmptyMemories'
+import { Footer } from '../components/Footer'
+import { Hero } from '../components/Hero'
+import { Signin } from '../components/Sigin'
 
 export default function Home() {
   return (
@@ -12,68 +13,15 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes pr-2"></div>
 
-        {/* Sign-In */}
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-stone-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            {' '}
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias
-          </p>
-        </a>
-
-        {/* Hero */}
-        <div className="space-y-5">
-          <Image src={logo} alt="Logo do NLW Spacetime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-600"
-            href="#"
-          >
-            Cadastrar lembrança
-          </a>
-        </div>
-
-        {/* Footer */}
-        <footer className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="underline hover:text-gray-100"
-            href="https://rocketseat.com.br"
-          >
-            Rocketseat
-          </a>
-        </footer>
+        <Signin />
+        <Hero />
+        <Footer />
       </div>
 
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a className="underline hover:text-gray-50" href="#">
-              criar agora
-            </a>
-            !
-          </p>
+          <EmptyMemories />
         </div>
       </div>
     </main>
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,11 @@
+export function EmptyMemories() {
+  return (
+    <p className="w-[360px] text-center leading-relaxed">
+      Você ainda não registrou nenhuma lembrança, comece a{' '}
+      <a className="underline hover:text-gray-50" href="#">
+        criar agora
+      </a>
+      !
+    </p>
+  )
+}
