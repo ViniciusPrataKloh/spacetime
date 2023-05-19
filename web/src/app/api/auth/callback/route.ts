3b0a2b214c95a26d3cf122bd219b32d2e77724e6
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-19 10:28:50 -0300

    Complete oauth authorization and cookie storage

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url)
+    const code = searchParams.get('code')
+
+    const registerResponse = await api.post('/register', {
+        code,
+    })
+
+    const { token } = registerResponse.data
+
+    const redirectUrl = new URL('/', request.url)
+
+    return NextResponse.redirect(redirectUrl, {
+        headers: {
+            'Set-cookie': `token=${token}; Path=/;`
+        }
+    })
+}
