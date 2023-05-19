ebafdc04e38692ae328229889e8266cf54569052
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-19 19:03:34 -0300

    Implementation of logout route

---
+++ web/src/app/api/auth/logout/route.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    const redirectUrl = new URL('/', request.url)
+
+    return NextResponse.redirect(redirectUrl, {
+        headers: {
+            'Set-cookie': `token=''; Path=/; max-age=0`
+        }
+    })
+}
