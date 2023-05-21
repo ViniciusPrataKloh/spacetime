30cbd90fa634c98805e48468ef6b5695a65750d3
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-21 15:46:37 -0300

    Create new memory

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
 export function EmptyMemories() {
   return (
     <p className="w-[360px] text-center leading-relaxed">
       Você ainda não registrou nenhuma lembrança, comece a{' '}
-      <a className="underline hover:text-gray-50" href="#">
+      <Link className="underline hover:text-gray-50" href="/memories/new">
         criar agora
-      </a>
+      </Link>
       !
     </p>
   )
